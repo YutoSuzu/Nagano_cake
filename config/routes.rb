f5bceb676e730ef9e983501178868d4517832ab9
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-07-11 15:30:40 +0000

    [add]device

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
+  
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
