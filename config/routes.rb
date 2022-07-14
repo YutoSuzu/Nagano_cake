c37203d329175e40b1d39c152cd317e5a96208c7
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-07-14 14:54:30 +0000

    [add]admin_model custmer_model gem_refile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  # devise_for :customers
+  # devise_for :admins
+  
   namespace :admin do
     get 'homes/top'
   end
-  
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
