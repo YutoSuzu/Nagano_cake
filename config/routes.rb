d209595fc0770135c77b25b7471432776abf023f
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-07-16 14:50:42 +0000

    [Add]admin,public_view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  # devise_for :customers
-  # devise_for :admins
-  
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sesstions: 'admin/sesstions'
+  }
+
   namespace :admin do
-    get 'homes/top'
+  
   end
 
   namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+  
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
