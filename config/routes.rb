17bb2ac1a13448f1dea82c6266fb3ad542125660
YutoSuzu <104748069+YutoSuzu@users.noreply.github.com>
2022-07-18 16:02:59 +0000

    ログイン画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,12 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-  
+    get '/' => 'homes#top'
+
   end
 
   namespace :public do
-  
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
